172be42e967f8d9af2e9a4142436ba4891a8359c
Pat <lifeisnoise@gmail.com>
2022-08-22 16:46:33 -0500

    First checkin

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import pygame
+import random
+
+
+def main():
+    image = pygame.image.load('faerun2.jpg')
+    pygame.init()
+    white = (255, 255, 255)
+    red = (255, 0, 0)
+    image_size = image.get_size()
+    X = image_size[0]
+    Y = image_size[1]
+    position = (random.randint(0, X), random.randint(0, Y))
+    display_surface = pygame.display.set_mode((X, Y))
+    pygame.display.set_caption('Faerun')
+
+    while True:
+        display_surface.fill(white)
+        display_surface.blit(image, (0, 0))
+        pygame.draw.circle(display_surface, red, position, 10)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
